ec9862998da028eb660283b27703d0e6a0ee97ac
Angel <angeldm03@hotmail.com>
2018-01-27 18:26:38 -0800

    Canvas sizing is correctly done. Blocks are drawn and randomly colored

--- GameLogic.js
+++ GameLogic.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded",function(){
     var g_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     var g_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     var context = canvas.getContext("2d");
-    canvas.width = g_width*.5;
+    canvas.width = g_height*.5;
     canvas.height = g_height;
 
     /*** Initialize game logic variables ***/
@@ -15,15 +15,16 @@ document.addEventListener("DOMContentLoaded",function(){
     const colors = {1:"#00f",2:"#f00",3:"#0f0",4:"#0ff",5:"#f0f",6:"#ff0"}
 
     /*** Initialize visual elements ***/
-    var brick_border = canvas.width*.05;
-    var brick_size = canvas.width/max_cols - brick_border;
-    Log(brick_border);
-    Log(brick_size);
+    var brick_border = canvas.height*.005;
+    var brick_offset_left = canvas.height * .005;
+    var brick_size = canvas.height/max_rows - brick_border;
+    Log("brick_border: "+brick_border);
+    Log("brick_size: " +brick_size);
     
     var i = -1;
 
     drawBricks();
-    drawTitle();
+    //drawTitle();
     draw();
 
     function draw() {
@@ -36,11 +37,11 @@ document.addEventListener("DOMContentLoaded",function(){
     function drawBricks() {
         for(var row = 0; row < max_rows; row++){
             for(var col = 0; col < max_cols; col++){
-                var brickX = col*brick_size + brick_border/2;
-                var brickY = row*brick_size + brick_border/2;
+                var brickX = col*(brick_size + brick_border);
+                var brickY = row*(brick_size + brick_border);
                 context.beginPath();
                 context.rect(brickX, brickY, brick_size, brick_size);
-                var clr = Math.floor(Math.random()*6+1);
+                var clr = Math.floor(Math.random()*max_cols+1);
                 context.fillStyle = colors[clr];
                 context.fill();
                 context.closePath();
