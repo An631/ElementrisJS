8a472b3f36048dc1421ea488c5e39a67bdbf3af8
Angel <angeldm03@hotmail.com>
2018-01-27 23:20:35 -0800

    Implemented autoresizing of canvas and elements in it

---
+++ BlocksStartTemplates.js
@@ -0,0 +1,198 @@
+var templates = {
+    "0": [
+        [  
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": false}
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false }
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": false},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true}
+        ]
+        ,[  
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true},
+            {"x": 0, "y": 0, "color": 1, "exists": true}
+        ]
+    ],
+    "1": [
+        [
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": false }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": false },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true }
+        ]
+        , [
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true },
+            { "x": 0, "y": 0, "color": 1, "exists": true }
+        ]
+    ] 
+}
--- GameLogic.js
+++ GameLogic.js
@@ -1,4 +1,8 @@
-document.addEventListener("DOMContentLoaded",function(){
+/*
+    ELEMENTRIS
+    Author: Angel Duran
+*/
+document.addEventListener("DOMContentLoaded",function(){    
     /*** Initialize code helper variables ***/
     var debugMode = true;
     const priority = {
@@ -6,17 +10,12 @@ document.addEventListener("DOMContentLoaded",function(){
         P: 1
     }
 
-    /***Initialize the canvas***/
-    var canvas = document.getElementById("gameCanvas");
-    // Every element size should be based on these two variables
-    var g_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var g_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    g_height = g_height*.90;
+/***Initialize Game***/
+    var canvas = document.getElementById("game_canvas");
     var context = canvas.getContext("2d");
-    canvas.width = g_height*.5;
-    canvas.height = g_height;
-
-    /*** Initialize game logic variables ***/
+    var g_height;
+    var block_border;
+    var block_size;
     const max_rows = 12;
     const max_cols = 6;
     const colors = {
@@ -27,45 +26,60 @@ document.addEventListener("DOMContentLoaded",function(){
         5:"#f0f",
         6:"#ff0"}
     var blocks = [];
+    calculateDisplayDimensions();
     initializeBlocks();
+    window.addEventListener("resize",onResize);
 
-    /*** Initialize visual elements ***/
-    var block_border = canvas.height*.005;
-    var block_offset_left = canvas.height * .005;
-    var block_size = canvas.height/max_rows - block_border;
-    Log("block_border: "+block_border,priority.D);
-    Log("block_size: " +block_size,priority.D);
-    
-    /*** Game Execution ***/
-    drawBlocks();
+        
+/*** Game Execution ***/
     draw();
-
+    // Create a draw function loop using animation frames.
+    requestAnimationFrame(draw);
     
-    /*** Game's Logic Functions ***/
+/*** Game's Logic Functions ***/
     function initializeBlocks() {
         // Load a block template that contains 2d array with exists predefined.
+        var randomTemplate = Math.floor(Math.random()*2);
+        Log("Chose template #: "+randomTemplate);
+        blocks = templates[randomTemplate];
         for (var row = 0; row < max_rows; row++) {
-            blocks[row] = [];
             for (var col = 0; col < max_cols; col++) {
-                blocks[row][col] = { x: 0, y: 0, color: "", exists: true };
+                if (!blocks[row][col].exists) continue;
+                var blockX = col * (block_size + block_border);
+                var blockY = row * (block_size + block_border);
+                var clr = Math.floor(Math.random() * max_cols + 1);
+                blocks[row][col].x = blockX;
+                blocks[row][col].y = blockY;
+                blocks[row][col].color = clr;
+            }
+        }
+    }
+
+    function updateBlocksPositions(){
+        for (var row = 0; row < max_rows; row++) {
+            for (var col = 0; col < max_cols; col++) {
+                if (!blocks[row][col].exists) continue;
+                var blockX = col * (block_size + block_border);
+                var blockY = row * (block_size + block_border);
+                blocks[row][col].x = blockX;
+                blocks[row][col].y = blockY;
             }
         }
     }
 
     function draw() {
-        Log("In Draw", priority.D);   
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawBlocks();  
     }
     
     function drawBlocks() {
         for(var row = 0; row < max_rows; row++){
             for(var col = 0; col < max_cols; col++){
-                if(!blocks[row][col]) continue;
-                var blockX = col*(block_size + block_border);
-                var blockY = row*(block_size + block_border);
-                var clr = Math.floor(Math.random()*max_cols+1);
-                blocks[row][col].x=blockX;
-                blocks[row][col].y=blockY;
-                blocks[row][col].color=clr;
+                if(!blocks[row][col].exists) continue;
+                var blockX = blocks[row][col].x;
+                var blockY = blocks[row][col].y;
+                var clr = blocks[row][col].color;
+
                 context.beginPath();
                 context.rect(blockX, blockY, block_size, block_size);
                 context.fillStyle = colors[clr];
@@ -73,18 +87,32 @@ document.addEventListener("DOMContentLoaded",function(){
                 context.closePath();
             }
         }
+    }  
+
+/*** Utility Functions ***/
+    function calculateDisplayDimensions() {
+        // Every element size should be based on these two variables
+        g_height = Math.min(document.documentElement.clientHeight, window.innerHeight || 0);
+        g_height = g_height * .90;
+        canvas.height = g_height;
+        canvas.width = g_height * .5;
+        block_border = canvas.height * .005;
+        block_size = canvas.height / max_rows - block_border;
+        Log("block_border: " + block_border, priority.D);
+        Log("block_size: " + block_size, priority.D);
     }
 
-    function drawTitle(){
-        context.fillStyle = "blue";
-        context.font = "bold 16px arial";
-        context.fillText("ELEMENTRIS", canvas.width/2 - 50, 16);
+    function onResize() {
+        Log("window width: "+window.innerWidth);
+        Log("window height: " + window.innerHeight);
+        Log("clientHeight: " + document.documentElement.clientHeight);
+        calculateDisplayDimensions();
+        updateBlocksPositions();
+        draw();
     }
-    
-    function Log(message, pri){
+
+    function Log(message, pri=priority.D){
         if(pri === priority.D && debugMode === false) return;
         window.console.log(message);
     }
-    // Create a draw function loop using animation frames.
-    requestAnimationFrame(draw);
 })
